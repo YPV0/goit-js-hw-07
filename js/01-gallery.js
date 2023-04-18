819afac717a07edc5070dc0392bdaa0d7e34a027
YPV0 <yuriisfw@gmail.com>
2023-04-18 21:53:59 +0300

    task1 complete

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,38 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+// Change code below this line
+const galleryRef = document.querySelector(".gallery");
+const galleryMarkup = createGalleryMarkup(galleryItems);
+galleryRef.insertAdjacentHTML("beforeend", galleryMarkup);
+galleryRef.addEventListener("click", onImgClick);
+
+function onImgClick(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  const instance = basicLightbox
+    .create(
+      `<img src="${event.target.dataset.source}" width="800" height="600">`
+    )
+    .show();
+}
+
+function createGalleryMarkup(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `
+      <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </li>
+    `;
+    })
+    .join("");
+}
