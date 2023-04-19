e03199da6cd4b9e676ab338b4a61800cfd22e085
YPV0 <yuriisfw@gmail.com>
2023-04-19 22:12:01 +0300

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -3,13 +3,12 @@ import { galleryItems } from "./gallery-items.js";
 const galleryRef = document.querySelector(".gallery");
 const galleryMarkup = createGalleryMarkup(galleryItems);
 galleryRef.insertAdjacentHTML("beforeend", galleryMarkup);
-galleryRef.addEventListener("click", onImgClick);
 
 function createGalleryMarkup(items) {
   return items
     .map(({ preview, original, description }) => {
       return `
-      <li class="gallery__item">
+         <li class="gallery__item">
         <a class="gallery__link" href="${original}">
           <img
             class="gallery__image"
@@ -18,18 +17,12 @@ function createGalleryMarkup(items) {
           />
         </a>
       </li>
-    `;
+      `;
     })
     .join("");
 }
 
-function onImgClick(event) {
-  event.preventDefault();
-  if (event.target.nodeName !== "IMG") {
-    return;
-  }
-  const lightbox = new SimpleLightbox(".gallery a", {
-    captionsData: "alt",
-    captionDelay: 250,
-  });
-}
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
